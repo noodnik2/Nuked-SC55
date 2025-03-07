4c8ba01f4a179d50f863c4ed63d0a7f1eca447c9
J.C. Moyer <jcmoyer32@gmail.com>
2025-03-07 06:09:17 -0500

    cmake: replace default find_package message

--- cmake/Findrtmidi.cmake
+++ cmake/Findrtmidi.cmake
@@ -1,8 +1,7 @@
 # First try to use a cmake build if there is one
-find_package(rtmidi CONFIG)
+find_package(rtmidi QUIET CONFIG)
 
 if(${rtmidi_FOUND})
-
     # The rtmidi find_package config wants us to #include <rtmidi/RtMidi.h>
     # which is different from how documentation and pkg-config does it.
     get_target_property(RTMIDI_INCLUDE_DIR RtMidi::rtmidi INTERFACE_INCLUDE_DIRECTORIES)
@@ -10,6 +9,13 @@ if(${rtmidi_FOUND})
 
     set_target_properties(RtMidi::rtmidi PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${RTMIDI_INCLUDE_DIR}"
+    )
+
+    get_target_property(RTMIDI_LIBRARY RtMidi::rtmidi LOCATION)
+
+    message(STATUS "Using cmake-enabled rtmidi:
+        Include: ${RTMIDI_INCLUDE_DIR}
+        Library: ${RTMIDI_LIBRARY}"
     )
 
 else()
@@ -25,5 +31,17 @@ else()
         INTERFACE_LINK_LIBRARIES "${RTMIDI_LIBRARY}"
     )
 
+    message(STATUS "Using rtmidi:
+        Include: ${RTMIDI_INCLUDE_DIR}
+        Library: ${RTMIDI_LIBRARY}"
+    )
+
 endif()
 
+if(rtmidi_FIND_REQUIRED)
+    if(NOT (RTMIDI_INCLUDE_DIR AND RTMIDI_LIBRARY))
+        message(FATAL_ERROR "Failed to locate rtmidi. Either:\n"
+            "1. Build and install rtmidi using cmake, then set CMAKE_PREFIX_PATH so that it contains the install directory\n"
+            "2. If you have a generic rtmidi installation, set RTMIDI_INCLUDE_DIR and RTMIDI_LIBRARY manually\n")
+    endif()
+endif()
